634ffbd7943c2bca8fff222ab094707e19be0c89
Emilems <emile.massau@gmail.com>
2020-08-13 18:28:47 +0200

    updated functions

---
+++ data/owner_emile.py
@@ -0,0 +1,29 @@
+class Owner():
+
+    def get_name(self):
+        self.first_name = input("Enter your first name: \n")
+        self.last_name = input("Enter your last name: \n")
+
+    def get_birthdate(self):
+        # TODO: Get birthdate of the owner dd-mm-yyyy
+        pass
+
+    def get_email(self):
+        # TODO: Get the email
+        pass
+
+    def get_phone(self):
+        # TODO: Get phonenumber
+        pass
+
+    def get_adress(self):
+        # TODO: Get city
+        # TODO: Get zip
+        # TODO: Street name
+        # TODO: Number
+        # TODO: Country
+        pass
+
+    def accept_termsofuse(self):
+        # TODO accept terms of use
+        pass
---
+++ main/create_account_emile.py
@@ -0,0 +1,33 @@
+from Catbnbs.data import owner_emile
+from infrastructure.switchlang import switch
+
+def main():
+    print("Welcome to CatBnb, hosted by Kiddy. \n")
+    show_commands()
+    action = input(" ")
+
+    while True
+        action = input("")
+
+        if action.lower() == "C":
+            pass
+
+        if action.lower() == "L":
+            pass
+
+        if action.lower() == "X":
+            pass
+
+        if action.lower() == "B":
+            pass
+        else:
+            print("Please choose a valid option")
+
+def show_commands():
+    print("What would you like to do? ")
+    print("[C]reate an account")
+    print("[L]ogin to your account")
+    print("E[X]it")
+    print("[B]ook a cage")
+
+main()
