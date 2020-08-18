b3dcc3771bc2017a9cd23c095b5ae298e2764165
Emilems <emile.massau@gmail.com>
2020-08-18 20:33:10 +0200

    Created owner_emile.py

--- database/infrastructure_emile.py
+++ database/infrastructure_emile.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://dominiquems:Domi-2492@cluster0.xxqtl.azure.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://dominiquems:Domi-2492@cluster0.xxqtl.azure.mongodb.net/catbnb?retryWrites=true&w=majority")
 
 db = client.get_database('catbnb')
 
-records = db.student_records
+records = db.cats
 
 # insert record
 
@@ -12,9 +12,9 @@ records = db.student_records
 def insert_record(entry):
     if not check_record(entry):
         records.insert_one(entry)
-        print(f"{entry['name'].capitalize()} has been succesfully added to Catbnb!")
+        print("Inserted record")
     else:
-        print(f"{entry['name'].capitalize()} is already in the database! \n")
+        print("Record already found in database")
 
 
 def check_record(entry: dict):
@@ -23,7 +23,7 @@ def check_record(entry: dict):
     return False
 
 
-def get_info():
+def get_info_cat():
     name = input("What is the name of the cat? \n").strip().lower()
     age = input("What is the age of the cat? \n").strip()
     gender = input("Is the gender [m] or [f]? \n").strip().lower()
@@ -32,18 +32,26 @@ def get_info():
 
 def delete_record(entry: dict):
     if not check_record(entry):
-        print(f"{entry['name'].capitalize()} is not found in the database.")
+        print("Not found in the database.")
         print(entry)
     else:
         records.delete_one(entry)
-        print(f"{entry['name'].capitalize()} has been succesfully removed from Catbnb!")
+        print("Succesfully removed from Catbnb!")
+
+
+def update_record(entry: dict):
+    if not check_record(entry):
+        print(f"{entry['name'].capitalize()} is not found in the database.")
+    else:
+        records.update()
+
 
 def main():
     choice = input("Do you want to [A]dd, [R]emove or [U]pdate you cat? \n").lower().strip()
     if choice == "a":
-        insert_record(get_info())
+        insert_record(get_info_cat())
     elif choice == "r":
-        delete_record(get_info())
+        delete_record(get_info_cat())
     elif choice == "p":
         pass
     elif choice == "u":
